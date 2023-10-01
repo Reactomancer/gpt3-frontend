edc1661c7ae3372b1038ae1f9a3458250eda3029
equsix <omar.m.zaid@hotmail.com>
2023-10-01 06:57:32 +0500

    adding header and brand and features

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,7 +1,17 @@
 import "./feature.css";
 
-function Feature() {
-  return <div></div>;
+function Feature({ title, text }) {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div></div>
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Feature;
